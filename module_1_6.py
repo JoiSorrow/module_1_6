50da1c20337310701a3b134fbca0add1597958f5
JoiSorrow <begunova921@gmail.com>
2024-10-10 16:51:14 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,15 @@
+# my_dict = {'Anna': 2001, 'Egor': 1992, 'Boris': 2005, 'Olga': 2001}
+# print (my_dict)
+# print(my_dict['Egor'])
+# print(my_dict.get('Nikol'))
+# my_dict.update({'Irina': 2003, 'Mary': 2007})
+# del my_dict ['Irina']
+# my_dict['Irina'] = 2003
+# print(my_dict['Irina'])
+# print(my_dict)
+my_set = {1, 235.236, "Anna", 1, "Pen", "Anna", (1, "string", 235.236)}
+print(my_set)
+my_set.add(56)
+my_set.add("tree")
+my_set.pop()
+print(my_set)
